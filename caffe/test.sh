d0b2daf7db1017680b41da418fb7618b02fb570e
Andy Liu <liuvay@gmail.com>
2019-01-03 11:23:46 +0800

    update

--- caffe/test.sh
+++
@@ -1,1 +0,0 @@
-/home/andy/caffe/build/tools/caffe test -model /home/andy/caffe/examples/mydata/apa_slot/test.prototxt -weights /home/andy/caffe/examples/mydata/apa_slot/models/apa_solver_iter_30000.caffemodel -gpu 0
---
+++ caffe/test_lmdb.sh
@@ -0,0 +1,4 @@
+/home/andy/caffe/build/tools/caffe test \
+-model /home/andy/caffe/examples/mydata/apa_slot/test.prototxt \
+-weights /home/andy/caffe/examples/mydata/apa_slot/models/apa_solver_iter_10000.caffemodel \
+-gpu 0
