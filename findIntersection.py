8f3dfa6968e0965df1fda7b677c8fd401676e8b3
Andy <liuvay@gmail.com>
2021-08-09 16:19:53 +0800

    Update findIntersection.py

--- findIntersection.py
+++ findIntersection.py
@@ -1,7 +1,16 @@
 
-def findIntersection(x1,y1,x2,y2,x3,y3,x4,y4):
+def findIntersection1(x1,y1,x2,y2,x3,y3,x4,y4):
     px = ((x1*y2 - y1*x2) * (x3-x4) - (x1-x2) * (x3*y4 - y3*x4)) / ((x1-x2) * (y3-y4) - (y1-y2) * (x3-x4))
     py = ((x1*y2 - y1*x2) * (y3-y4) - (y1-y2) * (x3*y4 - y3*x4)) / ((x1-x2) * (y3-y4) - (y1-y2) * (x3-x4))
+    return (px, py)
+
+def findIntersection2(p1, p2, p3, p4):
+    """
+    p1, p2 on the same line
+    p3, p4 on the another line
+    """
+    px = ((p1.x*p2.y - p1.y*p2.x) * (p3.x-p4.x) - (p1.x-p2.x) * (p3.x*p4.y - p3.y*p4.x)) / ((p1.x-p2.x) * (p3.y-p4.y) - (p1.y-p2.y) * (p3.x-p4.x))
+    py = ((p1.x*p2.y - p1.y*p2.x) * (p3.y-p4.y) - (p1.y-p2.y) * (p3.x*p4.y - p3.y*p4.x)) / ((p1.x-p2.x) * (p3.y-p4.y) - (p1.y-p2.y) * (p3.x-p4.x))
     return (px, py)
 
 def findIntersectionArray(x, y):
