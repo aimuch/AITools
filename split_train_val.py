6f52142b5d6f7a3442d0dde1234f98cd43a792eb
aimuch <liuvay@gmail.com>
2022-07-16 10:44:55 +0800

    update scripts

--- split_train_val.py
+++ split_train_val.py
@@ -17,27 +17,6 @@ import argparse
 import random
 from tqdm import tqdm
 
-val_rate = 0.2
-
-train_img = "./train_img"
-train_label = "./train_label"
-val_img = "./val_img"
-val_label = "./val_label"
-if os.path.exists(train_img):
-        shutil.rmtree(train_img)
-os.makedirs(train_img)
-
-if os.path.exists(train_label):
-        shutil.rmtree(train_label)
-os.makedirs(train_label)
-
-if os.path.exists(val_img):
-        shutil.rmtree(val_img)
-os.makedirs(val_img)
-
-if os.path.exists(val_label):
-        shutil.rmtree(val_label)
-os.makedirs(val_label)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -45,7 +24,7 @@ def parse_args():
     parser.add_argument('srcimg', help='images directory', type=str)
     parser.add_argument('train', default='./train', help='train directory', type=str)
     parser.add_argument('val', default='./val', help='test directory', type=str)
-    parser.add_argument('val_rate', default=0.2 help='val rate', type=float)
+    parser.add_argument('val_rate', default=0.2, help='val rate', type=float)
 
     args = parser.parse_args()
     return args
@@ -74,6 +53,7 @@ def pick(srclabel, srcimg, train_path, val_path, val_rate):
 
     labellists = os.listdir(srclabel)
     random.shuffle(labellists)
+
     val_num = int(len(labellists)*val_rate)
     if val_num <= 50:
         val_num = 50
@@ -90,28 +70,27 @@ def pick(srclabel, srcimg, train_path, val_path, val_rate):
         img_path = os.path.join(srcimg, label_info[0]+".jpg")
         if(not os.path.exists(img_path)):
             img_path = os.path.join(srcimg, label_info[0]+".png")
+
         if label in valset:
-            label_path_dst = os.path.join(val_label, os.path.basename(label_path))
-            img_path_dst = os.path.join(val_img, os.path.basename(img_path))
+            label_path_dst = os.path.join(os.path.join(val_path, 'ann'), os.path.basename(label_path))
+            img_path_dst = os.path.join(os.path.join(val_path, 'img'), os.path.basename(img_path))
             shutil.copyfile(label_path, label_path_dst)
             shutil.copyfile(img_path, img_path_dst)
         else:
-            label_path_dst = os.path.join(train_label, os.path.basename(label_path))
-            img_path_dst = os.path.join(train_img, os.path.basename(img_path))
+            label_path_dst = os.path.join(os.path.join(train_path, 'ann'), os.path.basename(label_path))
+            img_path_dst = os.path.join(os.path.join(train_path, 'img'), os.path.basename(img_path))
             shutil.copyfile(label_path, label_path_dst)
             shutil.copyfile(img_path, img_path_dst)
 
 if __name__ == '__main__':
     args = parse_args()
-    srclabel = args.srclabel
-    srcimg = args.srcimg
 
-    if not os.path.exists(srclabel):
-        print("Error !!! %s is not exists, please check the parameter"%srclabel)
+    if not os.path.exists(args.srclabel):
+        print("Error !!! %s is not exists, please check the parameter"%args.srclabel)
         sys.exit(0)
-    if not os.path.exists(srcimg):
-        print("Error !!! %s is not exists, please check the parameter"%srcimg)
+    if not os.path.exists(args.srcimg):
+        print("Error !!! %s is not exists, please check the parameter"%args.srcimg)
         sys.exit(0)
 
-    pick(srclabel, srcimg)
+    pick(args.srclabel, args.srcimg, args.train, args.val, args.val_rate)
     print("Done!")
